014bf5e7d0e766e3011e1d60e4e333dbd80dfbff
mrsajadpp <mrsajadpp@gmail.com>
2025-04-30 19:14:02 +0530

    Refactor search form: change button type to 'button' and reorder input elements

--- app/page.tsx
+++ app/page.tsx
@@ -24,10 +24,10 @@ export default function Maintenance() {
               </a>
             </div>
             <form className="input_container" action={"/search"} method="get">
-              <input type="text" placeholder="Search for websites" name="query" />
-              <button type="submit">
+              <button type="button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search-icon lucide-search"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>
               </button>
+              <input type="text" placeholder="Search for websites" name="query" />
             </form>
           </div>
         </div>
