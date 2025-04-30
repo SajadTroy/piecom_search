188d6eb424ad835275f9022ebdc8b5509eead15d
mrsajadpp <mrsajadpp@gmail.com>
2025-04-30 20:19:55 +0530

    Change submit button type to button in SearchForm component for improved functionality

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -291,7 +291,7 @@ export default function SearchForm() {
                 action="/search"
                 method="get"
             >
-                <button type="submit">
+                <button type="button">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
